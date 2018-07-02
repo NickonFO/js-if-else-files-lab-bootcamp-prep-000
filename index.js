772c59100940f11c34ddcbee659760fde2d0b42a
Nickon Fath-Ordoubadi <nickonfo@gmail.com>
2018-07-02 11:47:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 var name = "Joe"
 var height = "74"
 
-if (name === "Susan")
-  {
-    console.log("name is correct")
-  }
-else 
-  console.log("Expected name to be Susan")
+
 
 
 // Don't worry about this
